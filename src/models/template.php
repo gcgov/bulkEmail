a6d79801c1835c3ad47361e1ed6c64c4a5e93d1f
Andrew Sauder <andrewsauder@gmail.com>
2023-10-19 14:20:40 -0400

    add emergency services template

--- src/models/template.php
+++ src/models/template.php
@@ -5,5 +5,6 @@ enum template: string {
 
 	case countyTemplate2023 = 'countyTemplate2023';
 	case sheriffTemplate2023 = 'sheriffTemplate2023';
+	case emergencyServicesTemplate2023 = 'emergencyServicesTemplate2023';
 
 }
