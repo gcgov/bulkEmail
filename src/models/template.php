c8e80f44308da638d37b1dd9113bd9ba32923f03
Andrew Sauder <andrew@sauder.email>
2025-09-11 10:13:01 -0400

    add additional templates

--- src/models/template.php
+++ src/models/template.php
@@ -5,6 +5,8 @@ enum template: string {
 
 	case countyTemplate2023 = 'countyTemplate2023';
 	case sheriffTemplate2023 = 'sheriffTemplate2023';
+	case emergencyManagementTemplate2023 = 'emergencyManagementTemplate2023';
 	case emergencyServicesTemplate2023 = 'emergencyServicesTemplate2023';
+	case emergencyCommunicationsTemplate2023 = 'emergencyCommunicationsTemplate2023';
 
 }
